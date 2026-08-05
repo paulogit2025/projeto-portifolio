b15cd3d83a231446e4c2721451e8e5172400471f
paulogit2025 <paulo900@yahoo.com.br>
2026-08-05 12:41:12 -0300

    Menu funcionando

--- scripts/script.js
+++ scripts/script.js
@@ -9,10 +9,10 @@ temaEscuro(temasalvo === 'escuro');
 function temaEscuro(tipo) {
   if (tipo == true) {
     body.classList.add('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    // botao.innerHTML = '<i class="material-icons"></i>';
   } else {
     body.classList.remove('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    // botao.innerHTML = '<i class="material-icons"></i>';
   }
 }
 
